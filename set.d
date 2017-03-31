7c3f3f9a0a48779f155d465726424caf24c8c5b6
Ethan Reker <ethanepr@hotmail.com>
2017-03-31 18:28:22 -0500

    Update set.d

--- set.d
+++ set.d
@@ -1,3 +1,28 @@
 module set;
-class Set(T){    
-  T[] elements;    void insert(T element){        if(!this.contains(element))            elements ~= element;    }    T get(int index){        return elements[index];    }    bool contains(T element){        bool has = false;        foreach(T; elements){            if(T == element)                has = true;        }        return has;    }    T[] arr(){        return elements;    }}
+
+class Set(T){
+  
+  private T[] elements; 
+  
+  void insert(T element){        
+    if(!this.contains(element))            
+      elements ~= element;    
+  }   
+  
+  T get(int index){        
+    return elements[index];    
+  }    
+  
+  bool contains(T element){       
+    bool has = false;        
+    foreach(T; elements){            
+      if(T == element)                
+        has = true;        
+    }        
+    return has;   
+  }    
+  
+  T[] arr(){        
+    return elements;    
+  }
+}
