8170867b32fed4d7151237288cfdf496081822fc
Ethan Reker <ethanepr@hotmail.com>
2017-03-31 19:33:03 -0500

    Update set.d

--- set.d
+++ set.d
@@ -9,9 +9,28 @@ class Set(T){
       elements ~= element;    
   }   
   
+  //Remove element from the set.
+  void erase(T element){
+    int index = this.find(element);
+    if(index != -1)
+      elements = elements[0..index]~elements[index+1..elements.length];
+  }
+  
+  void clear(){
+    elements = new T[];
+  }
+  
   T get(int index){        
     return elements[index];    
   }    
+  
+  //Returns the index of the given element in the set. If the element doesn't
+  //exist, return -1
+  int find(T element){
+    for(int i = 0; i < elements.length; ++i)
+      if (elements[i] == element) return i;
+    return -1;
+  }
   
   bool contains(T element){       
     bool has = false;        
@@ -20,7 +39,16 @@ class Set(T){
         has = true;        
     }        
     return has;   
-  }    
+  }   
+  
+  bool empty(){
+    if(elements.length==0) return true;
+    else return false;
+  }
+  
+  int size(){
+    return elements.length;
+  }
   
   T[] arr(){        
     return elements;    
