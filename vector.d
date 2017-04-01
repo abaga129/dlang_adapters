c28b5c48e6c57cb8f6d89c14ce6f3b239bf50fdd
Ethan Reker <ethanepr@hotmail.com>
2017-03-31 22:00:48 -0500

    Update vector.d

--- vector.d
+++ vector.d
@@ -1,1 +1,28 @@
 module vector;
+class Vector(T){    
+  T[] elements; 
+  
+  void pushBack(T element){        
+    elements ~= element;    
+  }
+  
+  @property void popBack(){
+    --elements.length;
+  }
+  
+  T at(size_t index){
+    return elements[index];
+  }
+  
+  @property T front(){
+    return elements[0];
+  }
+  
+  @property T back(){
+    return elements[elements.length];
+  }
+  
+  @property T[] arr(){        
+    return elements;    
+  }
+}
