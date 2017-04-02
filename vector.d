37e2bb20de7e5d5ad066ef805f8b57db33b476be
Ethan Reker <ethanepr@hotmail.com>
2017-04-01 20:38:01 -0500

    Update vector.d

--- vector.d
+++ vector.d
@@ -1,4 +1,17 @@
+/**********************************************
+ * Author(s): Ethan Reker, ctrecordingslive
+ * Date: April 01, 2017
+ * License: use freely for any purpose
+ *
+ * Examples:
+ * --------------------------------------------
+ * Vector!int vect;
+ * vect.pushBack(3) //pushes 3 onto the vector
+ * --------------------------------------------
+ *
+ *********************************************/
 module vector;
+
 class Vector(T){    
   T[] elements; 
   
@@ -6,7 +19,7 @@ class Vector(T){   
     elements ~= element;    
   }
   
-  @property void popBack(){
+  @property void popBack(){ 
     --elements.length;
   }
   
@@ -15,6 +28,7 @@ class Vector(T){   
   }
   
   @property T front(){
+    
     return elements[0];
   }
   
@@ -22,7 +36,16 @@ class Vector(T){   
     return elements[elements.length];
   }
   
+  /* Returns the elements in the vector as a dynamic array
+   * The elements can then be accessed using the [] operator
+   * example:
+   * ----------------------------------------------------------
+   * int val = vect.arr[0] ///Get element at index 0 in vector 
+   *                       ///and assign it to val.
+   * ----------------------------------------------------------
+   */
   @property T[] arr(){        
     return elements;    
   }
 }
+ 
