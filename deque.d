5106cd28c33782982293236014a2624156d0f0f8
Ethan Reker <ethanepr@hotmail.com>
2017-04-01 20:44:01 -0500

    Create deque.d

---
+++ deque.d
@@ -0,0 +1,15 @@
+/**
+This module is an implementation of a double-ended queue based on 
+std/containers/dlist
+
+Authors: Ethan Reker, ctrecordingslive@gmail.com
+Date: April 01, 2017
+*/
+module adapters.deque;
+
+@safe unittest
+{
+
+}
+
+
