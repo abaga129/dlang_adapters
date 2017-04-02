a6df1eef1b802da3ed4b5b7c0d4ff91c31009a5e
Ethan Reker <ethanepr@hotmail.com>
2017-04-01 22:13:25 -0500

    Update deque.d

--- deque.d
+++ deque.d
@@ -9,7 +9,51 @@ module adapters.deque;
 
 @safe unittest
 {
-
+  import adapters.deque;
+  
 }
 
 
+class Deque (T)
+{
+  import std.cointainers.dlist;
+  
+  private DList!T dlist;
+  
+  this.()
+  {
+    dlist = DList!T.init;
+  }
+  
+  public
+  {
+    
+/**
+Pushes $(D stuff) onto the front of the Deque. 
+*/
+    @property void pushBack(Stuff)(Stuff stuff)
+    {
+      dlist.insertFront(stuff);
+    }
+    
+    @property void pushFront (Stuff) (Stuff stuff)
+    {
+      dlist.insertBack(stuff);
+    }
+    
+    @property T front()
+    {
+      return dlist.front;
+    }
+    
+    @property T back()
+    {
+      return dlist.back;
+    }
+    
+    @property bool empty()
+    {
+      return dlist.empty;
+    }
+  }
+}
