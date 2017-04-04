93763388aaf928873bebb6cb4049427b71907cf3
Ethan Reker <ethanepr@hotmail.com>
2017-04-04 10:55:21 -0500

    Update queue.d

--- adapter/queue.d
+++ adapter/queue.d
@@ -1,6 +1,41 @@
 module adapter.queue;
 
-struct Queue
+struct Queue (T)
 {
+  private import std.container.dlist;
+  
+  private DList!T queue;
 
+  public
+  {
+    @property void push (Stuff) (Stuff stuff)
+    {
+      queue.insertBack(stuff);
+    }
+    
+    @property void pop()
+    {
+      queue.removeFront;
+    }
+    
+    @property T front()
+    {
+      return queue.front;
+    }
+    
+    @property T back()
+    {
+      return queue.back;
+    }
+    
+    @property ulong size()
+    {
+      return queue.length;
+    }
+    
+    @property bool empty()
+    {
+      return queue.empty;
+    }
+  }
 }
