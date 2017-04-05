077e61cdc00d633010020aa319edf3efc5786e2c
Ethan Reker <ethanepr@hotmail.com>
2017-04-04 22:56:29 -0500

    Update matrix.d

--- adapter/matrix.d
+++ adapter/matrix.d
@@ -2,5 +2,41 @@ module adapter.matrix;
 
 struct Matrix (T)
 {
+  T[][] mat;
+  int m;
+  int n;
+  
+  this(int m, int n)
+  {
+    this.m = m;
+    this.n = n;
+    
+    mat.length = m;
+    for(int i = 0; i < m; ++i)
+    {
+      mat[i].length = n;
+    }
+  }
+  
+  Matrix multiply(Matrix A)
+  {
+  }
+  
+  @property Matrix inverse()
+  {
+    
+  }
+  
+  @property T[][] elem()
+  {
+    return &mat;
+  }
+}
+
+Matrix multiply(Matrix LHS, Matrix RHS)
+{
+  assert(LHS.m == RHS.n);
   
 }
+
+Matrix inverse(Matrix 
